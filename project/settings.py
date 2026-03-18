8e57ede4d82ab34f162ccda4401d068da51c55b3
Isabelle Mosca <bellemosca@gmail.com>
2026-03-18 15:24:39 -0300

    feat: Integrate Whitenoise for efficient static file serving in Django.

--- project/settings.py
+++ project/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -159,6 +160,15 @@ STATIC_ROOT = BASE_DIR / ".static/"
 MEDIA_ROOT = BASE_DIR / ".media/"
 
 STATICFILES_DIRS = [BASE_DIR / "shared/static/"]
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 # Django Bootstrap 5
